764245893a23fe6100672da996a21dd91f583d38
Christian Dias <christianltdias@gmail.com>
2024-02-03 12:52:11 -0300

    enhancements in dates selections

--- components/pickers/calendar/calendar.tsx
+++ components/pickers/calendar/calendar.tsx
@@ -1,28 +1,29 @@
 import { useState } from "react";
 import { DayOfMonth, DayOfWeek } from "../../../types/dates";
 import { createDate, createMonthArray } from "../../../utils/date.utils";
-import styles from "./calendar.module.sass";
 import { concatStyles } from "../../../utils/styles.utils";
-import Tooltip from "../../tooltip/tooltip";
+import styles from "./calendar.module.sass";
 
 type CalendarProps = {
   multipleSelect?: boolean;
+  allowPastSelect?: boolean;
 };
 
-export default function Calendar({ multipleSelect }: CalendarProps) {
+export default function Calendar({ multipleSelect = false, allowPastSelect = true}: CalendarProps) {
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
   const [firstDate, setFirstDate] = useState<DayOfMonth>(null);
   const [secondDate, setSecondDate] = useState<DayOfMonth>(null);
 
+  const day = currentDate.getDate();
   const month = currentDate.getMonth();
   const year = currentDate.getFullYear();
 
   var monthMatrix = createMonthArray(month, year);
   var daysOfWeekSequence = Object.values(DayOfWeek);
-  // console.log(monthMatrix)
+
   const checkIfInRange = (
     dayOfMonth: DayOfMonth
-  ): "edge-left" | "edge-right" | "range" | "none" => {
+  ): "edge-left" | "edge-right" | "edge-both" | "range" | "none" => {
 
     const initialDate = firstDate
       ? createDate(firstDate.day, firstDate.month, firstDate.year).getTime()
@@ -32,14 +33,11 @@ export default function Calendar({ multipleSelect }: CalendarProps) {
       : null;
     const date = createDate(dayOfMonth.day, dayOfMonth.month, dayOfMonth.year).getTime()
 
-    // if(dayOfMonth.day === 1 || dayOfMonth.day === 30){
-    //   console.log(`---------------${dayOfMonth.day}-${dayOfMonth.month}----------------`)
-    //   console.log(initialDate)
-    //   console.log(finalDate)
-    //   console.log(date)
-    // }
-
-    if (initialDate === date) {
+    if(initialDate === date && (finalDate === date || !finalDate)
+      || (initialDate === date && DayOfWeek.Saturday === DayOfWeek[dayOfMonth.dayOfWeek])
+      || (finalDate === date && DayOfWeek.Sunday === DayOfWeek[dayOfMonth.dayOfWeek])){
+      return "edge-both";
+    } else if (initialDate === date) {
       return "edge-left";
     } else if (finalDate === date) {
       return "edge-right";
@@ -66,7 +64,8 @@ export default function Calendar({ multipleSelect }: CalendarProps) {
     if (!secondDate) {
       if (date < initialDate) {
         setFirstDate(dayOfMonth);
-      } else {
+        setSecondDate(firstDate);
+      } else {setSecondDate
         setSecondDate(dayOfMonth);
       }
       return;
@@ -74,13 +73,20 @@ export default function Calendar({ multipleSelect }: CalendarProps) {
 
     const finalDate = createDate(secondDate.day, secondDate.month, secondDate.year).getTime();
     
-    if (date <= initialDate || (date > initialDate && date < finalDate)) {
+    if(date === initialDate && date === finalDate){
+      return;
+    } else if (date < initialDate || date === finalDate) {
       setFirstDate(dayOfMonth);
-    } else if (date === finalDate || date === initialDate) {
-      setFirstDate(dayOfMonth);
-      setSecondDate(dayOfMonth);
+    } else if (date > finalDate || date === initialDate) {
+      setSecondDate(dayOfMonth)
     } else {
-      setSecondDate(dayOfMonth);
+      var initialOffset = (date - initialDate) / (1000 * 3600 * 24) + 1
+      var finalOffset = (finalDate - date) / (1000 * 3600 * 24) + 1
+      if(initialOffset <= finalOffset){
+        setFirstDate(dayOfMonth)
+      } else {
+        setSecondDate(dayOfMonth)
+      }
     }
   };
 
@@ -88,17 +94,12 @@ export default function Calendar({ multipleSelect }: CalendarProps) {
     <div className={styles["calendar-container"]}>
       <div className={styles["days-wrapper"]}>
         {daysOfWeekSequence.map((dayOfWeek) => (
-          <div className={styles["day-cell-week"]}>{dayOfWeek}</div>
+          <div key={`day-week-${dayOfWeek}`} className={styles["day-cell-week"]}>{dayOfWeek}</div>
         ))}
         {monthMatrix.map((dayOfMonth) => (
-          // <Tooltip text="Right click to remove selection">
             <div
-              className={concatStyles(
-                dayOfMonth.month !== month
-                  ? styles["day-cell-preview"]
-                  : styles["day-cell"],
-                styles[`day-cell-selected-${checkIfInRange(dayOfMonth)}`]
-              )}
+              key={`day-month-${dayOfMonth.month}-${dayOfMonth.day}`}
+              className={dayOfMonth.month !== month ? styles["day-cell-preview"] : styles["day-cell"]}
               onClick={() => handleSelection(dayOfMonth)}
               onContextMenu={(e) => {
                 e.preventDefault();
@@ -108,9 +109,12 @@ export default function Calendar({ multipleSelect }: CalendarProps) {
                 }
               }}
             >
-              {dayOfMonth.day}
+              <div className={concatStyles(
+                styles[`day-cell-selected-${checkIfInRange(dayOfMonth)}`],
+                day === dayOfMonth.day && month === dayOfMonth.month && year === dayOfMonth.year ? styles['today'] : '')}>
+                {dayOfMonth.day}
+              </div>
             </div>
-          // </Tooltip>
         ))}
       </div>
     </div>
